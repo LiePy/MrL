6b1c50fbb6728640dd21a89456c3f50404eb7af4
wanglei <7360324+lie_py@user.noreply.gitee.com>
2021-01-15 08:41:46 +0800

    加入了语音合成功能SDK，测试Demo成功，但是没有自定义去调用他

--- app/src/main/java/com/baidu/aip/asrwakeup3/mrl/MainActivity.java
+++ app/src/main/java/com/baidu/aip/asrwakeup3/mrl/MainActivity.java
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.baidu.aip.asrwakeup3.core.mini.ActivityMiniWakeUp;
+import com.baidu.aip.asrwakeup3.mrl.sample.MiniActivity;
 
-public class MainActivity extends ActivityMiniWakeUp {
+public class MainActivity extends MiniActivity {
 
 
 }
--- app/src/main/java/com/baidu/aip/asrwakeup3/mrl/SettingsActivity.java
+++ app/src/main/java/com/baidu/aip/asrwakeup3/mrl/SettingsActivity.java
@@ -1,11 +1,18 @@
 package com.baidu.aip.asrwakeup3.mrl;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceFragmentCompat;
+
+import java.util.ArrayList;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,8 +20,8 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-//        Intent intent= new Intent(this, MainActivity.class);
-//        startActivity(intent);
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -26,6 +33,8 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        initPermission();
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -45,5 +54,39 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    /**
+     * android 6.0 以上需要动态申请权限
+     */
+    private void initPermission() {
+        String[] permissions = {
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                Manifest.permission.WRITE_SETTINGS,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        ArrayList<String> toApplyList = new ArrayList<>();
+
+        for (String perm : permissions) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, perm)) {
+                toApplyList.add(perm);
+                // 进入到这里代表没有权限.
+            }
+        }
+        String[] tmpList = new String[toApplyList.size()];
+        if (!toApplyList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, toApplyList.toArray(tmpList), 123);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // 此处为android 6.0以上动态授权的回调，用户自行实现。
     }
 }
